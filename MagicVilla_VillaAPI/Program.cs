40387065e542bcfa838dbe60a44892afc51f8df1
jmbp1999 <pamudithaj98@gmail.com>
2023-05-10 13:36:07 +0530

    main changes to program

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,11 +1,26 @@
+using MagicVilla_VillaAPI.Logging;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(option =>
+{
+    option.ReturnHttpNotAcceptable = true;
+});
+
+//Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo
+//    .File("log/villaLogs.txt", rollingInterval: RollingInterval.Year).CreateLogger();
+
+
+//builder.Host.UseSerilog();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ILogging, Logging>();
+//lets say we want a new LogginV2 to be injected.but with same Log function and different implementation.what we do is just have to inject to hre
+//we do not have to create new objects everywhere.
 
 var app = builder.Build();
 
